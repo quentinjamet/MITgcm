0b9f5e5e89471ca5aad1d8615058f0db1e163508
Quentin Jamet <qjamet@moe.ocean.fsu.edu>
2024-05-15 10:48:10 -0400

    forgot call to advec_ke in __init__.py ...

--- utils/python/MITgcmutils/MITgcmutils/__init__.py
+++ utils/python/MITgcmutils/MITgcmutils/__init__.py
@@ -1,9 +1,9 @@
 from numpy import NaN, Inf
 from .mds import rdmds, wrmds
 from .ptracers import iolabel,iolabel2num
-from .diagnostics import readstats, advec_vm, advec_um
+from .diagnostics import readstats, advec_vm, advec_um, advec_ke
 from .mnc import rdmnc, mnc_files
 
 __all__ = ['NaN', 'Inf', 'rdmds', 'wrmds', 'iolabel', 'iolabel2num',
-           'readstats', 'advec_vm', 'advec_um', 'rdmnc', 'mnc_files', 'cs', 'llc']
+           'readstats', 'advec_vm', 'advec_um', 'advec_ke', 'rdmnc', 'mnc_files', 'cs', 'llc']
 
